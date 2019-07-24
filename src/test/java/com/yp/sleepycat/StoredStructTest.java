13cffc529787225643e566e56354e3749fde4af7
yepan <yepanknow@qq.com>
2019-07-24 19:20:34 +0800

    实现模式——状态部分

--- src/test/java/com/yp/sleepycat/StoredStructTest.java
+++ src/test/java/com/yp/sleepycat/StoredStructTest.java
@@ -1,34 +1,11 @@
 package com.yp.sleepycat;
 
 import com.sleepycat.bind.EntryBinding;
-import com.sleepycat.bind.tuple.IntegerBinding;
 import com.sleepycat.bind.tuple.TupleBinding;
 import com.sleepycat.collections.StoredList;
-import com.sleepycat.je.Database;
-import com.sleepycat.je.DatabaseConfig;
-import com.sleepycat.je.Environment;
-import com.sleepycat.je.EnvironmentConfig;
-import org.junit.Before;
 import org.junit.Test;
-import java.io.File;
 import static org.junit.Assert.*;
-public class StoredStructTest {
-
-    private String home = "F:\\data\\sleepycat";
-    private Database database;
-
-    @Before
-    public void beforeEachMethod() {
-        EnvironmentConfig config = new EnvironmentConfig();
-        config.setAllowCreate(true);
-
-        Environment environment = new Environment(new File(home), config);
-
-        DatabaseConfig databaseConfig = new DatabaseConfig();
-        databaseConfig.setAllowCreate(true);
-
-        database = environment.openDatabase(null, "storedStructTest", databaseConfig);
-    }
+public class StoredStructTest extends DatabaseTest {
 
     @Test
     public void testStoredList() {
