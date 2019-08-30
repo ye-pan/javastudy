c3114b4c787508a16795993b0d8a74838cc5c3b0
yepan <yepanknow@qq.com>
2019-08-30 22:11:52 +0800

    Thread，Executor API测试

---
+++ src/test/java/com/yp/concurrent/ThreadAPITest.java
@@ -0,0 +1,66 @@
+package com.yp.concurrent;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ThreadAPITest {
+
+    private static final Logger log = LoggerFactory.getLogger(ThreadAPITest.class);
+
+    @Test
+    public void testInterrupt() throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                //此处为什么为false，看下Thread.sleep方法的注释，InterruptedException有说明
+                assertFalse(Thread.currentThread().isInterrupted());
+            }
+        });
+        thread.start();
+        thread.interrupt();
+        thread.join();
+    }
+
+    @Test
+    public void testWait() throws InterruptedException {
+        final int timeToWait = 3000;
+        final int timeToSleep = 5000;
+        Object mutex = new Object();
+        Thread thread = new Thread(() -> {
+           synchronized (mutex) {
+               try {
+                   long start = System.currentTimeMillis();
+                   mutex.wait(timeToWait);
+                   long end = System.currentTimeMillis() - start;
+                   assertTrue(end >= timeToSleep);
+                   log.debug("time wait done...");
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               }
+           }
+        });
+        Thread thread2 = new Thread(() -> {
+            synchronized (mutex) {
+                try {
+                    long start = System.currentTimeMillis();
+                    Thread.sleep(timeToSleep);
+                    long end = System.currentTimeMillis() - start;
+                    assertTrue(end >= timeToSleep);
+                    log.debug("sleep done...");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        thread.join();
+        thread2.start();
+        thread2.join();
+    }
+}
--- src/test/java/com/yp/concurrent/ThreadPoolExecutorTest.java
+++ src/test/java/com/yp/concurrent/ThreadPoolExecutorTest.java
@@ -1,11 +1,14 @@
 package com.yp.concurrent;
 
+import org.checkerframework.checker.units.qual.C;
 import org.junit.Assert;
 import org.junit.Test;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 
 public class ThreadPoolExecutorTest {
 
@@ -33,13 +36,42 @@ public class ThreadPoolExecutorTest {
             }
             //有任务队列的执行池，只有任务数量大于任务队列数量的时候，才会开启新线程去处理任务，并且新线程数量不会超过最大线程数
             if(i > (maximumPoolSize + taskQueueSize)) {
-                Assert.assertEquals(maximumPoolSize, executor.getActiveCount());
+                assertEquals(maximumPoolSize, executor.getActiveCount());
             }
             if(i > taskQueueSize && i < (maximumPoolSize + taskQueueSize)) {
                 Assert.assertTrue(executor.getActiveCount() > corePoolSize);
             }
             if(i < maximumPoolSize) {
-                Assert.assertEquals(corePoolSize, executor.getActiveCount());
+                assertEquals(corePoolSize, executor.getActiveCount());
+            }
+        }
+
+        latch.await();
+    }
+
+    @Test
+    public void testFixedThreadPool() throws InterruptedException {
+        int nThread = 5;
+        int taskCount = 100;
+        CountDownLatch latch = new CountDownLatch(taskCount);
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(nThread, nThread, 5, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+        for(int i = 0; i < taskCount; i++) {
+            executor.submit(() -> {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            });
+            if(i < nThread) {
+                //在未达到corePoolSize之前，会创建线程来执行对应任务
+                assertEquals(i + 1, executor.getActiveCount());
+            } else {
+                //达到corePoolSize后，先往任务队列里面放
+                assertEquals(nThread, executor.getActiveCount());
             }
         }
 
